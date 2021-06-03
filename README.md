149e3d2d2613d03242be72423ef793781a6eb737
jamesmart77 <jamesmart77@gmail.com>
2021-06-02 21:32:17 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CrossPoint Christian Academy
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h4>
+          Welcome to CrossPoint Christian Academy!
+        </h4>
+        <p>
+          We are currently under construction but come back soon!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
