6f3aee0fec7e9dc414090fb389a3646c0e3b3346
jamesmart77 <jamesmart77@gmail.com>
2021-06-14 06:32:32 -0400

    loading toast

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
 import Landing from '../pages/Landing';
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function App() {
+
+  useEffect(() => {
+    toast.info(`We are still under construction so please pardon our appearance 🤓 
+    Send us an email if you would like to schedule an information session.`, {
+      position: "bottom-right",
+      autoClose: 10000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      });
+  })
+
   return (
     <BrowserRouter>
       <div className="app-container">
@@ -15,6 +28,8 @@ export default function App() {
           </Switch>
           <Footer />
         </main>
+        
+        <ToastContainer />
       </div> 
     </BrowserRouter>
   );
